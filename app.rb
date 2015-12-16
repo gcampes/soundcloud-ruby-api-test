cf31f696d5f9ad46b988004f31ffb12fea61997c
Gabriel Campes <gabrieldecampes@gmail.com>
2015-12-16 11:19:51 -0200

    Change button color, change playlists

--- app.rb
+++ app.rb
@@ -6,12 +6,17 @@ require 'json'
 
 class SoundCloudApiApp < Sinatra::Base
   get '/' do
-    client = Soundcloud.new(:client_id => 'API_KEY')
-    playlists = client.get('/playlists', :user_id => 47611670)
+    client = Soundcloud.new(:client_id => 'CLIENT_ID')
+    playlists = client.get('/playlists', :user_id => 171442550)
     html = []
 
-    playlists.each { |playlist| html.push(client.get('/oembed', :url => playlist.permalink_url)['html'])}
-
+    playlists.each { |playlist| html.push(client.get('/oembed', :url => playlist.permalink_url)['html']) }
+    html.each {|embed|
+      puts embed
+      embed.sub! 'true', 'false'
+      embed.sub! '">', '&color=000000&show_playcount=false&sharing=false&download=false&show_user=false">'
+      puts embed
+    }
     erb :index, :locals => {:playlists => playlists, :html => html}
   end
 end
